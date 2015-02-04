7bb7a03547c656220dfd45a2f515d44e77a47be1
Randall Miller <randallsm83@gmail.com>
2015-02-03 21:49:42 -0800

    Separate aliases

--- .make.sh
+++ .make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc oh-my-zsh tmux.conf"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc oh-my-zsh tmux.conf aliases"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -30,5 +30,5 @@ for file in $files; do
     ln -s $dir/.$file ~/.$file
 done
 
-cd $dir/bundle
+cd $dir/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git
