c9909446b8fd2c79cc07f7a91dd1fe7951c812ca
Randall Miller <randallsm83@gmail.com>
2024-10-31 01:10:00 -0700

    add lsp servers

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -579,14 +579,19 @@ require('lazy').setup({
                 --    https://github.com/pmizio/typescript-tools.nvim
                 --
                 -- But for many setups, the LSP (`ts_ls`) will work just fine
+                vimls = {},
+                html = {},
+                jsonls = {},
                 ts_ls = {},
                 eslint = {},
+                quick_lint_js = {},
                 bashls = {
                     -- cmd = {...},
                     filetypes = {'sh', 'zsh'}
                     -- capabilities = {},
                     -- settings = { ... },
                 },
+                perlnavigator = {},
                 lua_ls = {
                     -- cmd = {...},
                     -- filetypes = { ...},
