1f7ee94d8968a0d7950c23ed998b8614f9c3e0fd
Randall Miller <randallsm83@gmail.com>
2024-11-11 16:55:36 -0800

    update setup script

--- install/setup.sh
+++ install/setup.sh
@@ -1,66 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export CACHEDIR="$HOME/.cache"
-export CONFIGDIR="$HOME/.config"
-export LOCALDIR="$HOME/.local"
-export DATADIR="$LOCALDIR/share"
-export STATEDIR="$LOCALDIR/state"
-export TMPDIR="${TMPDIR:-/tmp}"
-export BIN_DIR="$LOCALDIR/bin"
-export BINDIR=$BIN_DIR
-
-export XDG_CACHE_HOME="$CACHEDIR"
-export XDG_CONFIG_HOME="$CONFIGDIR"
-export XDG_BIN_HOME="$BINDIR"
-export XDG_DATA_HOME="$DATADIR"
-export XDG_STATE_HOME="$STATEDIR"
-export XDG_RUNTIME_DIR="$TMPDIR"
-
-export DOTFILES="$HOME/.dotfiles"
-
-export HOMEBREW_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/Homebrew"
-export HOMEBREW_BUNDLE_USER_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/Homebrew"
-export HOMEBREW_LOGS="${XDG_CACHE_HOME:-$HOME/.cache}/Homebrew/Logs"
-export HOMEBREW_TEMP="${XDG_RUNTIME_DIR:-/tmp}/Homebrew"
-
-cores="$(sysctl -n hw.ncpu)"
-export HOMEBREW_MAKE_JOBS="$cores"
-export HOMEBREW_DISPLAY_INSTALL_TIMES=1
-export HOMEBREW_COLOR=1
-
-export HOMEBREW_NO_INSECURE_REDIRECT=1
-export HOMEBREW_CURL_RETRIES=3
-
-export HOMEBREW_NO_ANALYTICS=1
+source_env() {
+  [[ -r "$HOME/.dotfiles/env/dot-config/env.d/dirs.sh" ]] && source "$HOME/.dotfiles/env/dot-config/env.d/dirs.sh"
+  [[ -r "$DOTFILES/env/dot-config/env.d/paths.sh" ]] && source "$DOTFILES/env/dot-config/env.d/paths.sh"
+  [[ -r "$DOTFILES/env/dot-config/env.d/homebrew.sh" ]] && source "$DOTFILES/env/dot-config/env.d/homebrew.sh"
+}
 
 install_homebrew() {
-  if [[ "$(uname)" == "Darwin" ]]; then
-
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-
-  elif [[ "$(uname)" == "Linux" ]]; then
-
-    git clone https://github.com/Homebrew/brew "$HOME/homebrew"
-    eval "$("$HOME/homebrew/bin/brew" shellenv)"
-
-    brew update --force --quiet
-    chmod -R go-w "$(brew --prefix)/share/zsh"
-
-  else
-    echo "Unsupported operating system. Exiting."
-    exit 1
-  fi
+  git clone https://github.com/Homebrew/brew "$HOMEBREW_PREFIX"
+  brew update --force --quiet
+  chmod -R go-w "$(brew --prefix)/share/zsh"
 }
+
+# Clone your dotfiles repository
+git clone https://github.com/Randallsm83/dotfiles.git "$DOTFILES"
+
+source_env
+echo "ENV files sourced."
 
 install_homebrew
 echo "Homebrew installed."
 
 # brew install stow
 # echo "Stow installed."
-
-# Clone your dotfiles repository
-git clone https://github.com/Randallsm83/dotfiles.git "$DOTFILES"
 
 echo "Temporary environment setup complete. Dotfiles repo has been cloned."
 echo "Now go install stow and stow the packages!"
