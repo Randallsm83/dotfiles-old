9b130df78d5bb7027e839d62a013d7f8b800a979
Randall Miller <randallsm83@gmail.com>
2024-11-12 00:51:38 -0800

    fix git config

--- install/setup.sh
+++ install/setup.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 source_env() {
-  [[ -r "$HOME/.dotfiles/env/dot-config/env.d/dirs.sh" ]] && source "$HOME/.dotfiles/env/dot-config/env.d/dirs.sh"
-  [[ -r "$DOTFILES/env/dot-config/env.d/paths.sh" ]] && source "$DOTFILES/env/dot-config/env.d/paths.sh"
-  [[ -r "$DOTFILES/env/dot-config/env.d/homebrew.sh" ]] && source "$DOTFILES/env/dot-config/env.d/homebrew.sh"
+  for env_file in "$HOME/.dotfiles/env/dot-config/env.d"/*.conf; do
+    echo "$env_file"
+    [ -r "$env_file" ] && source "$env_file"
+  done
+  [[ -r "$HOME/.dotfiles/homebrew/dot-config/env.d/50-homebrew.conf" ]] && source "$HOME/.dotfiles/homebrew/dot-config/env.d/50-homebrew.conf"
+  # [[ -r "$HOME/.dotfiles/env/dot-config/env.d/10-dirs.conf" ]] && source "$HOME/.dotfiles/env/dot-config/env.d/10-dirs.conf"
+  # [[ -r "$DOTFILES/env/dot-config/env.d/paths.sh" ]] && source "$DOTFILES/env/dot-config/env.d/paths.sh"
+  # [[ -r "$DOTFILES/env/dot-config/env.d/homebrew.sh" ]] && source "$DOTFILES/env/dot-config/env.d/homebrew.sh"
 }
 
 install_homebrew() {
