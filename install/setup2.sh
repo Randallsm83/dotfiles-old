1845b48e733fee95452440b589ea4982ca401329
Randall Miller <rmiller@yakko.sd.dreamhost.com>
2024-11-12 23:35:11 -0800

    update

--- install/setup2.sh
+++ install/setup2.sh
@@ -16,22 +16,21 @@ git clone https://github.com/Homebrew/brew "$HOMEBREW_PREFIX"
 eval "$("$HOMEBREW_PREFIX/bin/brew" shellenv)"
 
 # Step 3: Install Perlbrew locally to .local/perlbrew
-echo "Installing Perlbrew locally to $PERLBREW_ROOT..."
-curl -L https://install.perlbrew.pl | bash
-$(PERLBREW_ROOT)/bin/perlbrew init
-source "$PERLBREW_ROOT/etc/bashrc"
+#echo "Installing Perlbrew locally to $PERLBREW_ROOT..."
+#curl -L https://install.perlbrew.pl | bash
+#$(PERLBREW_ROOT)/bin/perlbrew init
+#source "$PERLBREW_ROOT/etc/bashrc"
 
 # Step 4: Install the latest Perl with Perlbrew
-echo "Installing the latest Perl with Perlbrew..."
-perlbrew install-cpanm
-perlbrew install-patchperl
-perlbrew install perl-5.40.0 --notest --thread -j 10
-perlbrew switch perl-5.40.0
+#echo "Installing the latest Perl with Perlbrew..."
+#perlbrew install-patchperl
+#perlbrew install-cpanm
+#perlbrew --notest install -n -j 10 --switch --thread stable -D cc=gcc
 
 # Step 5: Set up `local::lib` using Perlbrew’s built-in support
-echo "Configuring local::lib with Perlbrew..."
-perlbrew lib create perl54@local
-perlbrew switch perl54@local
+#echo "Configuring local::lib with Perlbrew..."
+#perlbrew lib create perl54@local
+#perlbrew switch perl54@local
 
 # Step 6: Install required Perl modules (Test::More and Test::Output)
 echo "Installing Test::More and Test::Output..."
@@ -39,7 +38,7 @@ cpanm Test::More Test::Output
 
 # Step 7: Use Homebrew to install GNU Stow
 echo "Installing GNU Stow with Homebrew..."
-brew install stow
+brew install -s stow
 
 # Step 8: Stow dotfiles
 echo "Stowing dotfiles..."
