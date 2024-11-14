aea87b62c54e5e110ae08944ca53d075794e5603
Randall Miller <randallsm83@gmail.com>
2024-11-14 10:27:40 -0800

    install rework

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ log() {
 
 # Check permissions early
 check_permissions() {
-  local dirs=("$XDG_DATA_HOME" "$XDG_CONFIG_HOME" "$XDG_STATE_HOME" "$XDG_CACHE_HOME")
+  local dirs=("$XDG_DATA_HOME" "$XDG_CONFIG_HOME" "$XDG_STATE_HOME" "$XDG_CACHE_HOME" "$LOG_DIR" "$BUILD_DIR")
 
   for dir in "${dirs[@]}"; do
     if [ ! -d "$dir" ]; then
