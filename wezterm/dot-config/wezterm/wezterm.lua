db3aa9c59bf8aba6e0d0e278c38595ce8432c6b2
Randall Miller <randallsm83@gmail.com>
2024-11-21 01:34:56 -0800

    no vi->vim

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -71,7 +71,7 @@ config.font = wezterm.font_with_fallback({
   "Hack Nerd Font Mono",
   "FiraCode Nerd Font Mono",
 })
-config.font_size = 13.5
+config.font_size = 14
 config.line_height = 1.1
 
 ------------------------- Tabs -----------------------------
