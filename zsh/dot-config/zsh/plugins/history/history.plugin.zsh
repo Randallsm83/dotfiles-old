bf7dfa14b2c8c9a1e8649de31ab2fb9e20e3f3c8
Randall Miller <randallsm83@gmail.com>
2024-12-31 21:23:01 -0800

    better history plugin. add alias-finder options

--- zsh/dot-config/zsh/plugins/history/history.plugin.zsh
+++ zsh/dot-config/zsh/plugins/history/history.plugin.zsh
@@ -1,48 +1,11 @@
-## History wrapper
-function omz_history {
-  # parse arguments and remove from $@
-  local clear list stamp REPLY
-  zparseopts -E -D c=clear l=list f=stamp E=stamp i=stamp t:=stamp
-
-  if [[ -n "$clear" ]]; then
-    # if -c provided, clobber the history file
-
-    # confirm action before deleting history
-    print -nu2 "This action will irreversibly delete your command history. Are you sure? [y/N] "
-    builtin read -E
-    [[ "$REPLY" = [yY] ]] || return 0
-
-    print -nu2 >| "$HISTFILE"
-    fc -p "$HISTFILE"
-
-    print -u2 History file deleted.
-  elif [[ $# -eq 0 ]]; then
-    # if no arguments provided, show full history starting from 1
-    builtin fc $stamp -l 1
-  else
-    # otherwise, run `fc -l` with a custom format
-    builtin fc $stamp -l "$@"
-  fi
-}
-
-# Timestamp format
-case ${HIST_STAMPS-} in
-  "mm/dd/yyyy") alias history='omz_history -f' ;;
-  "dd.mm.yyyy") alias history='omz_history -E' ;;
-  "yyyy-mm-dd") alias history='omz_history -i' ;;
-  "") alias history='omz_history' ;;
-  *) alias history="omz_history -t '$HIST_STAMPS'" ;;
-esac
-
 ## History file configuration
-ZSTATE="$XDG_STATE_HOME/zsh"
-if [[ ! -d $ZSTATE ]]; then
-    echo "Creating directory: $ZSTATE"
-    mkdir -p "$ZSTATE" || { echo "Failed to create history dir: $ZSTATE" >&2; return 1; }
-fi
+_state_dir=${XDG_STATE_HOME:-$HOME/.local/state}/zsh
+[[ -d "$_state_dir"  ]] || mkdir -p "$_state_dir"
 
-HISTFILE="$ZSTATE/zsh_history"
-HISTSIZE=1000000
+_zhistfile="$_state_dir/zsh_history"
+
+HISTFILE="$_zhistfile"
+HISTSIZE=10000
 SAVEHIST="$HISTSIZE"
 
 ## History command configuration
@@ -56,3 +19,5 @@ setopt hist_ignore_all_dups   # Delete old recorded entry if new entry is a dupl
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_reduce_blanks     # Remove unnecessary blanks
+
+unset _state_dir _zhistfile
